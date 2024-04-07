6b8784bce3e7f1d3e29fda535839c4dc6e0b9413
jianwu <1012245670@qq.com>
2024-04-07 12:19:17 +0800

    review rotate

---
+++ src/review/Array/rotate.java
@@ -0,0 +1,39 @@
+package review.Array;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class rotate {
+    class Solution {
+        public void rotate(int[] nums, int k) {
+            int n = nums.length;
+            k = k % n;
+            // 反转整个数组
+            reverse(nums, 0, n - 1);
+            // 反转前k个数组
+            reverse(nums, 0, k - 1);
+            // 反转剩余n-k个
+            reverse(nums, k, n - 1);
+        }
+        // 交换头尾
+        private void reverse(int[] nums, int start, int end){
+            while(start < end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate outer = new rotate();
+        Solution solution = outer.new Solution();
+        solution.rotate(nums, k);
+        for(int num : nums){
+            System.out.println(num + " ");
+        }
+    }
+}
