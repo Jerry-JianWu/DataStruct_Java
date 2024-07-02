d497c6c29b2c9b888c3c50bbe4856fdaeeb12bd2
jianwu <1012245670@qq.com>
2024-07-02 11:21:06 +0800

    jump2

---
+++ src/LeetCode100/greedy/jump.java
@@ -0,0 +1,25 @@
+package LeetCode100.greedy;
+
+public class jump {
+    class Solution{
+        public int jump(int[] nums){
+            if(nums == null || nums.length == 1){
+                return 0;
+            }
+            int n = nums.length;
+            int jumps = 0, currentEnd = 0, farthest = 0;
+            for (int i = 0; i < n - 1; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+                if( i == currentEnd){
+                    jumps++;
+                    currentEnd = farthest;
+                    if(currentEnd >= n - 1){
+                        break;
+                    }
+                }
+
+            }
+            return jumps;
+        }
+    }
+}
