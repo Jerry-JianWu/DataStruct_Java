e06fb2eb692f9fa936b7d1ddac7731ecd2fa6923
jianwu <1012245670@qq.com>
2024-08-02 10:30:30 +0800

    correct hasCycle

--- src/LeetCode100/LinkList/hasCycle1.java
+++ src/LeetCode100/LinkList/hasCycle1.java
@@ -4,11 +4,11 @@ public class hasCycle1 {
     // 快慢指针法
     public class Solution {
         public boolean hasCycle(ListNode head) {
-            if(head.next == null) return false;
+            if(head == null && head.next == null) return false;
             // 快指针比满指针多走一步，相遇则有环不相遇则无环，且快指针并且快指针下个是null则无环
             ListNode slow = head, fast = head;
                 // 如果快慢指针相遇则存在环
-            while(fast != null && fast.next == null){
+            while(fast != null && fast.next != null){
                 slow = slow.next;
                 fast = fast.next.next;
                 if (slow == fast){
---
+++ src/LeetCode100/minWindow.java
@@ -0,0 +1,44 @@
+package LeetCode100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class minWindow {
+
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> targetFreq = new HashMap<>();
+        Map<Character, Integer> windowFreq = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            targetFreq.put(c, targetFreq.getOrDefault(c, 0) + 1);
+        }
+        int left = 0, right = 0, start = 0, matchCount = 0;
+        int minLen = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+            right++;
+            if (targetFreq.containsKey(rightChar)) {
+                windowFreq.put(rightChar, windowFreq.getOrDefault(rightChar, 0) + 1);
+                if (windowFreq.get(rightChar).equals(targetFreq.get(rightChar))) {
+                    matchCount++;
+                }
+            }
+
+            while (matchCount == targetFreq.size()) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+                char leftChar = s.charAt(left);
+                left++;
+                if (targetFreq.containsKey(leftChar)) {
+                    if (targetFreq.get(leftChar).equals(windowFreq.get(leftChar))) {
+                        matchCount--;
+                    }
+                }
+            }
+
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
