8becedce4af62c7a679097f3b7dd6fc74968de39
RoronoaZoroWj <1012245670@qq.com>
2025-01-05 21:47:31 +0800

    modify groupAnagrams

--- src/LeetCode100/Hash/groupAnagrams.java
+++ src/LeetCode100/Hash/groupAnagrams.java
@@ -12,7 +12,7 @@ public class groupAnagrams {
             // 将排序后的字符数组转换为字符串，将字符串的值作为键存储
             String key = String.copyValueOf(charArray);
             // 若存在排序后的字符串键，则将当前字符串添加到键对应的值中
-            if(map.containsKey(key)){
+            if(!map.containsKey(key)){
                 map.get(key).add(s);
             }
             // 若不存在排序后的字符串键，则创建键值对，键为排序后的字符串，值为当前字符串
