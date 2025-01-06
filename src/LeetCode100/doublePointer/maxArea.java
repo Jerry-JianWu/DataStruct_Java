18869284ee681e5901a60ec027418ce4b4a1a400
RoronoaZoroWj <1012245670@qq.com>
2025-01-06 22:49:37 +0800

    maxArea

--- src/LeetCode100/doublePointer/maxArea.java
+++ src/LeetCode100/doublePointer/maxArea.java
@@ -1,17 +1,17 @@
 package LeetCode100.doublePointer;
 
 public class maxArea {
-    public int maxArea(int[] height){
+    public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
         int maxarea = 0;
-        while(left<right){
+        while (left < right) {
             int currentHeight = Math.min(height[left], height[right]);
             int currentWidth = right - left;
             int currentArea = currentHeight * currentWidth;
             maxarea = Math.max(maxarea, currentArea);
-            if(height[left] < height[right]){
+            if (height[left] < height[right]) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
