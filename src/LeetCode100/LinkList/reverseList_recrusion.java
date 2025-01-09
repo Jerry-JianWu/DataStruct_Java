c0a28a4fe69bb15075597cf92f101b3e6f57a444
RoronoaZoroWj <1012245670@qq.com>
2025-01-09 18:21:19 +0800

    modify reverseList_recrusion

--- src/LeetCode100/LinkList/reverseList_recrusion.java
+++ src/LeetCode100/LinkList/reverseList_recrusion.java
@@ -4,7 +4,7 @@ public class reverseList_recrusion {
     class Solution{
         public ListNode reverseList(ListNode head){
             // 递归终止条件：当前节点为空，或者下一个节点为空
-            if(head == null || head.next == null) return null;
+            if(head == null || head.next == null) return head;
             // 递归反转子链表
             ListNode newHead = reverseList(head.next);
             // 将当前节点的下一个节点的next指向当前节点
